1089fdb21a7ecc6ae96ac4091c9048fcd9f6e8ce
pikapika <aoxbtksgo13@naver.com>
2025-11-26 16:12:05 +0900

    #3 swagger 생성

---
+++ src/main/java/org/etmetmy/bn_server/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package org.etmetmy.bn_server.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi bnServerApi() {
+        return GroupedOpenApi.builder()
+                .group("bn-server")
+                .packagesToScan("org.etmetmy.bn_server")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI bnServerOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("isThisTeamTwoTeam API")
+                        .version("v1")
+                        .description("Backend API documentation for Team Two")
+                        .contact(new Contact()
+                                .name("Team Two")
+                                .email("teamtwo@example.com")));
+    }
+}
