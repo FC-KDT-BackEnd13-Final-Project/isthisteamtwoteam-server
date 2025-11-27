f1f1890429139ff0ce740b67c5cfa3b66efa72ea
김동균 <eastVirusDa@gmail.com>
2025-11-27 16:00:59 +0900

    feat:체크리스트 수정

--- src/main/java/org/etmetmy/bn_server/domain/checkList/service/CheckListService.java
+++ src/main/java/org/etmetmy/bn_server/domain/checkList/service/CheckListService.java
@@ -1,8 +1,11 @@
 package org.etmetmy.bn_server.domain.checkList.service;
 
+import org.etmetmy.bn_server.domain.checkList.dto.request.CheckListUpdateRequest;
 import org.etmetmy.bn_server.domain.checkList.dto.response.CheckListResponse;
 
 public interface CheckListService {
 
     CheckListResponse save();
+
+    CheckListResponse update(Long checkListId, CheckListUpdateRequest request);
 }
--- src/main/java/org/etmetmy/bn_server/domain/checkList/service/CheckListServiceImpl.java
+++ src/main/java/org/etmetmy/bn_server/domain/checkList/service/CheckListServiceImpl.java
@@ -2,11 +2,14 @@ package org.etmetmy.bn_server.domain.checkList.service;
 
 import lombok.RequiredArgsConstructor;
 import org.etmetmy.bn_server.domain.checkList.dto.request.CheckListCreateRequest;
+import org.etmetmy.bn_server.domain.checkList.dto.request.CheckListUpdateRequest;
 import org.etmetmy.bn_server.domain.checkList.dto.response.CheckListResponse;
 import org.etmetmy.bn_server.domain.checkList.entity.CheckList;
 import org.etmetmy.bn_server.domain.checkList.repository.CheckListRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +21,16 @@ public class CheckListServiceImpl implements CheckListService {
     public CheckListResponse save() {
         CheckList entity = CheckListCreateRequest.Converter.toEntity();
         CheckList saved = checkListRepository.save(entity);
+        return CheckListResponse.Converter.from(saved);
+    }
+
+    @Override
+    public CheckListResponse update(Long checkListId, CheckListUpdateRequest request) {
+
+        CheckList checkList = checkListRepository.findById(checkListId).orElse(null);
+        CheckList updateEntity =CheckListUpdateRequest.Converter.updateEntity(request, checkList);
+        CheckList saved = checkListRepository.save(updateEntity);
+
         return CheckListResponse.Converter.from(saved);
     }
 }
