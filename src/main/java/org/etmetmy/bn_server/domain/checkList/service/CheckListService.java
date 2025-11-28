a7cfeb438589363dde4f7306443b9189e80e8687
김동균 <eastVirusDa@gmail.com>
2025-11-28 20:19:41 +0900

    feat: OFFSET기반 페이지네이션 구현 및 체크리스트에 적용 & 검색
    # Conflicts:
    #	src/main/java/org/etmetmy/bn_server/domain/checkList/controller/CheckListController.java
    #	src/main/java/org/etmetmy/bn_server/domain/checkList/service/CheckListService.java
    #	src/main/java/org/etmetmy/bn_server/domain/checkList/service/CheckListServiceImpl.java

--- src/main/java/org/etmetmy/bn_server/domain/checkList/service/CheckListService.java
+++ src/main/java/org/etmetmy/bn_server/domain/checkList/service/CheckListService.java
@@ -2,6 +2,8 @@ package org.etmetmy.bn_server.domain.checkList.service;
 
 import org.etmetmy.bn_server.domain.checkList.dto.request.CheckListUpdateRequest;
 import org.etmetmy.bn_server.domain.checkList.dto.response.CheckListResponse;
+import org.etmetmy.bn_server.global.page.PageRequest;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -12,6 +14,11 @@ public interface CheckListService {
     CheckListResponse update(Long checkListId, CheckListUpdateRequest request);
 
     void delete(Long checkListId);
+
+    Page<CheckListResponse> getCheckLists(PageRequest pageRequest);
+
+    Page<CheckListResponse> searchCheckLists(String keyword, PageRequest pageRequest);
+}
 
 
     }
