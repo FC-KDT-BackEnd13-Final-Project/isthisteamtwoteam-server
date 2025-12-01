86dd1da165ef51a0deb55a91bf9b122465b8735f
김동균 <eastVirusDa@gmail.com>
2025-12-01 11:40:19 +0900

    feat: 진짜 충돌 수정

--- src/main/java/org/etmetmy/bn_server/domain/user/repository/UserRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package org.etmetmy.bn_server.domain.user.repository;
-
-import org.etmetmy.bn_server.domain.user.dto.MemberSearchCondition;
-import org.etmetmy.bn_server.domain.user.entity.User;
-import java.util.List;
-
-public interface UserRepositoryCustom {
-    List<User> search(MemberSearchCondition condition);
-}
