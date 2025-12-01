e6512ee5f1d4923aa06db82993348d37dad22e91
조현희 <dyahxhx@gmail.com>
2025-12-01 20:49:01 +0900

    중간 저장

--- src/main/java/org/etmetmy/bn_server/domain/file/repository/FileRepository.java
+++ src/main/java/org/etmetmy/bn_server/domain/file/repository/FileRepository.java
@@ -1,4 +1,18 @@
 package org.etmetmy.bn_server.domain.file.repository;
 
-public class FileRepository {
+import org.etmetmy.bn_server.domain.file.entity.File;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface FileRepository extends JpaRepository<File, Long> {
+
+    @Query(value = "SELECT f.* FROM file f " +
+                   "JOIN post p ON f.post_id = p.post_id " +
+                   "WHERE p.project_id = :projectId " +
+                   "AND f.is_deleted = false",
+           nativeQuery = true)
+    List<File> findByProjectId(@Param("projectId") Long projectId);
 }
--- src/main/java/org/etmetmy/bn_server/domain/file/service/FileServiceImpl.java
+++ src/main/java/org/etmetmy/bn_server/domain/file/service/FileServiceImpl.java
@@ -2,10 +2,12 @@ package org.etmetmy.bn_server.domain.file.service;
 
 import lombok.RequiredArgsConstructor;
 import org.etmetmy.bn_server.domain.file.dto.FileListDTO;
+import org.etmetmy.bn_server.domain.file.entity.File;
 import org.etmetmy.bn_server.domain.file.repository.FileRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +19,23 @@ public class FileServiceImpl implements FileService {
     @Override
     public List<FileListDTO> findAllByProjectId(Long projectId){
 
-        List<FileListDTO> files = fileRepository.findByProjectId(projectId);
+        List<File> files = fileRepository.findByProjectId(projectId);
+
+        return files.stream()
+                .map(file -> FileListDTO.builder()
+                        .fileId(file.getFileId())
+                        .fileTitle(file.getFileTitle())
+                        .filePath(file.getFilePath())
+                        .fileType(file.getFileType())
+                        .fileSize(file.getFileSize())
+                        .entityTypeName(file.getEntityType().getEntityType())
+                        .postId(file.getPost().getPostId())
+                        .uploadUserId(file.getUploadedBy())
+                        .uploadedAt(file.getCreatedAt())
+                        .Deleted(file.getIsDeleted())
+                        .deletedAt(file.getDeletedAt())
+                        .deleteUserId(file.getDeletedBy())
+                        .build())
+                .collect(Collectors.toList());
     }
 }
