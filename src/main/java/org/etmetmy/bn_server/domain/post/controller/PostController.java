abd9a99555bd3c8f22a40292b14823de134eb461
mmingoo <aldrn0323@naver.com>
2025-12-03 11:21:10 +0900

    PostController 주석 변경

--- src/main/java/org/etmetmy/bn_server/domain/post/controller/PostController.java
+++ src/main/java/org/etmetmy/bn_server/domain/post/controller/PostController.java
@@ -16,7 +16,7 @@ public class PostController {
 
     private final PostService postService;
 
-    // 1. 게시글 상세 조회
+    //todo: 게시글 상세 조회
     @GetMapping("/{projectId}/posts/{postId}")
     public ResponseEntity<CommonResponse<PostDetailResponse>> getPostDetail(
             @PathVariable Long projectId,
@@ -26,7 +26,7 @@ public class PostController {
         return ResponseEntity.ok(CommonResponse.success("게시글 조회 성공", response));
     }
 
-    // 2. 게시글 승인 API
+    //todo: 게시글 승인 API
     @PatchMapping("/{postId}/approval")
     public ResponseEntity<CommonResponse<Object>> approvePost(
             @PathVariable Long postId,
@@ -37,7 +37,7 @@ public class PostController {
         return ResponseEntity.ok(CommonResponse.success("게시글 승인 완료"));
     }
 
-    // 3. 게시글 거절 API
+    //todo: 게시글 거절 API
     @PatchMapping("/{postId}/reject")
     public ResponseEntity<CommonResponse<Object>> rejectPost(
             @PathVariable Long postId,
