eac18fedf6dd36217f7cb37e794d310e1705d20b
조현희 <dyahxhx@gmail.com>
2025-12-08 15:49:30 +0900

    불필요한 import 제거

--- src/main/java/org/etmetmy/bn_server/domain/file/service/FileService.java
+++ src/main/java/org/etmetmy/bn_server/domain/file/service/FileService.java
@@ -2,7 +2,6 @@ package org.etmetmy.bn_server.domain.file.service;
 
 import jakarta.servlet.http.HttpSession;
 import org.etmetmy.bn_server.domain.file.dto.ActiveFileListDTO;
-import org.etmetmy.bn_server.domain.file.dto.FileInfoDTO;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -15,5 +14,6 @@ public interface FileService {
     // 업로드 된 파일 삭제
     void deletePostFiles(Long projectId, Long postId, Long fileId, Long loginUserId);
 
+    // 임시 파일 업로드
     List<ActiveFileListDTO> postFiles(Long projectId, Long postId, List<MultipartFile> files);
 }
