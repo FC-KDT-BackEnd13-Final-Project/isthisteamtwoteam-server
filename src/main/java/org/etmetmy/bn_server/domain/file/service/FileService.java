f0cd46a428d4377d7474abf4e8ef0bb3772f4fd1
조현희 <dyahxhx@gmail.com>
2025-12-08 15:28:41 +0900

    feat: 임시 파일 업로드

--- src/main/java/org/etmetmy/bn_server/domain/file/service/FileService.java
+++ src/main/java/org/etmetmy/bn_server/domain/file/service/FileService.java
@@ -2,6 +2,8 @@ package org.etmetmy.bn_server.domain.file.service;
 
 import jakarta.servlet.http.HttpSession;
 import org.etmetmy.bn_server.domain.file.dto.ActiveFileListDTO;
+import org.etmetmy.bn_server.domain.file.dto.FileInfoDTO;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -13,4 +15,5 @@ public interface FileService {
     // 업로드 된 파일 삭제
     void deletePostFiles(Long projectId, Long postId, Long fileId, Long loginUserId);
 
+    List<ActiveFileListDTO> postFiles(Long projectId, Long postId, List<MultipartFile> files);
 }
--- src/main/java/org/etmetmy/bn_server/domain/post/service/PostServiceImpl.java
+++ src/main/java/org/etmetmy/bn_server/domain/post/service/PostServiceImpl.java
@@ -171,7 +171,6 @@ public class PostServiceImpl implements PostService {
         }
 
         // 5. 링크 처리
-
         List<Link> savedLinks = new ArrayList<>();
         if (requestDto.getLinkUrls() != null && !requestDto.getLinkUrls().isEmpty()) {
             for (String linkUrl : requestDto.getLinkUrls()) {
