3acce7604a7777f82f92396546730439c17e3f5b
pikapika <36557956+ejs10@users.noreply.github.com>
2025-12-08 16:34:26 +0900

    프로젝트 생성 내 개발사 검색 기능 (#94)
    * 프로젝트 생성 내 개발사 검색 기능
    
    * 프로젝트 생성 내 개발사 검색 기능 (키워드 검색 제거)

--- src/main/java/org/etmetmy/bn_server/domain/project/service/ProjectMemberService.java
+++ src/main/java/org/etmetmy/bn_server/domain/project/service/ProjectMemberService.java
@@ -1,5 +1,15 @@
 package org.etmetmy.bn_server.domain.project.service;
 
+import org.etmetmy.bn_server.domain.project.dto.response.ProjectMemberSearchResponse;
+
+import java.util.List;
+
 public interface ProjectMemberService {
     boolean hasRoleToProject(Long userId, Long projectId);
+
+    // 프로젝트 생성 - 개발사/고객사 담당자, 사원조회
+    List<ProjectMemberSearchResponse> searchDeveloperMembers();
+
+    // 프로젝트 설정 - 개발사 담당자, 사원 조회
+
 }
