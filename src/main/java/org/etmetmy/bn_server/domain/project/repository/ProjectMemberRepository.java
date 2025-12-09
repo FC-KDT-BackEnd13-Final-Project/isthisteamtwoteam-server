dc5ab5aba1d1f251d1339eeca08368242d2cd7f1
Hyunhee Cho <dyahxhx@gmail.com>
2025-12-09 15:04:21 +0900

    Reorder methods in ProjectMemberRepository

--- src/main/java/org/etmetmy/bn_server/domain/project/repository/ProjectMemberRepository.java
+++ src/main/java/org/etmetmy/bn_server/domain/project/repository/ProjectMemberRepository.java
@@ -14,10 +14,6 @@ import static org.etmetmy.bn_server.domain.user.entity.QUser.user;
 @Repository
 public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Long> {
 
-    List<ProjectMember> findByProjectId(Long projectId);
-    List<ProjectMember> findByProjectIdIn(List<Long> projectIds);
-    ProjectMember findByUserIdAndProjectId(Long userId, Long projectId);
-
     //사용자가 특정 프로젝트의 멤버인지 확인
     @Query("SELECT CASE WHEN COUNT(pm) > 0 THEN true ELSE false END " +
            "FROM ProjectMember pm " +
@@ -29,6 +25,10 @@ public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Lo
     // 프로젝트 + 유저 기준으로 매핑 삭제
     long deleteByProjectIdAndUserId(Long projectId, Long userId);
 
+    List<ProjectMember> findByProjectId(Long projectId);
+    List<ProjectMember> findByProjectIdIn(List<Long> projectIds);
+    ProjectMember findByUserIdAndProjectId(Long userId, Long projectId);
+    
     // 권한이 있는 모든 프로젝트 조회
     @Query("""
     select pm.project.id
