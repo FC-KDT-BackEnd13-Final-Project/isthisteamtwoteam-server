1dbc8e8feb4bc40c4453e820b1a1319a3f171094
조현희 <dyahxhx@gmail.com>
2025-12-09 11:05:26 +0900

    완료

--- src/main/java/org/etmetmy/bn_server/domain/project/dto/response/ProjectTrashResponse.java
+++ src/main/java/org/etmetmy/bn_server/domain/project/dto/response/ProjectTrashResponse.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.etmetmy.bn_server.domain.project.entity.Project;
 
 @Getter
 @NoArgsConstructor
--- src/main/java/org/etmetmy/bn_server/domain/project/repository/ProjectRepository.java
+++ src/main/java/org/etmetmy/bn_server/domain/project/repository/ProjectRepository.java
@@ -1,6 +1,5 @@
 package org.etmetmy.bn_server.domain.project.repository;
 
-import org.etmetmy.bn_server.domain.project.dto.response.ProjectTrashResponse;
 import org.etmetmy.bn_server.domain.project.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -10,7 +9,6 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
@@ -45,7 +43,4 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
             "WHERE p.id = :projectId")
     int updateProjectStage(@Param("projectId") Long projectId,
                            @Param("stageId") Long stageId);
-
-
-
 }
