987264feac8085b93797ef9c534383cfa0e36b1f
조현희 <dyahxhx@gmail.com>
2025-12-13 18:10:56 +0900

    Merge branch 'develop' into fix/#183-post-edit

--- src/main/java/org/etmetmy/bn_server/domain/post/repository/PostRepository.java
+++ src/main/java/org/etmetmy/bn_server/domain/post/repository/PostRepository.java
@@ -1,16 +1,11 @@
 package org.etmetmy.bn_server.domain.post.repository;
 
-import jakarta.persistence.LockModeType;
-import org.etmetmy.bn_server.domain.post.dto.response.PostListResponse;
 import org.etmetmy.bn_server.domain.post.entity.Post;
 import org.etmetmy.bn_server.domain.post.entity.RequestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
