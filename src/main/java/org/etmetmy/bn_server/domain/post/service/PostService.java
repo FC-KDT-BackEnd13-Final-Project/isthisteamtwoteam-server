d50e7f098c99eb3e04c1c6b56f64791d3721ead2
김동균 <eastVirusDa@gmail.com>
2025-12-16 14:47:51 +0900

    게시글 목록 조회 및 파일 목록 조회

--- src/main/java/org/etmetmy/bn_server/domain/post/service/PostService.java
+++ src/main/java/org/etmetmy/bn_server/domain/post/service/PostService.java
@@ -6,6 +6,9 @@ import org.etmetmy.bn_server.domain.post.dto.request.PostUpdateRequest;
 import org.etmetmy.bn_server.domain.post.dto.response.PostCreateResponse;
 import org.etmetmy.bn_server.domain.post.dto.response.PostDetailResponse;
 import org.etmetmy.bn_server.domain.post.dto.response.PostListByStageResponse;
+import org.etmetmy.bn_server.domain.post.dto.response.PostListResponse;
+
+import java.util.List;
 
 public interface PostService {
     PostDetailResponse getPostDetail(Long postId);
@@ -18,8 +21,7 @@ public interface PostService {
 
     PostCreateResponse createPost(Long projectId, @Valid PostCreateRequest requestDto, Long loginUserId);
 
-    PostCreateResponse updatePost(Long projectId, Long postId, @Valid PostUpdateRequest requestDto, Long loginUserId);
-    List<PostListResponse> getPostListByStage(Long projectId, String stage);
+    //PostCreateResponse updatePost(Long projectId, Long postId, @Valid PostUpdateRequest requestDto, Long loginUserId);
 
     PostCreateResponse updatePost(Long postId, @Valid PostUpdateRequest requestDto, Long loginUserId);
 
